b3fe623901fe462bd3c2ca808c7f10005bb35941
Ritika Mehta <55449612+ritikamehta11@users.noreply.github.com>
2024-10-14 13:14:36 -0400

    Update ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -16,7 +16,7 @@ export const ProductCard = ({ product }) => {
   return (
     <>
       <div className='flex flex-col border-2 min-w-40 p-4'>
-        <img src={`http://localhost:8888/${product.image}`} alt="product image" />
+        <img src={`https://flower-shop-backend-81tw.onrender.com/${product.image}`} alt="product image" />
       <p className='flex flex-row justify-between my-4'><span className='font-semibold'> {product.name} </span><span className='text-right'>{product.price}</span></p>
       <button className='sec-btn border-2 py-1' onClick={handleAddToCart}>Add to Cart</button>
       <Link to='/popup' className='text-xs mt-2 underline text-blue-700'>View Details</Link>
