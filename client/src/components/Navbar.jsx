bd2d425780fd53878c4d5066b67c02b02d7016d4
Ritika Mehta <mehtaritika@gmail.com>
2024-10-17 13:05:57 -0400

    console statement added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const { user } = useContext(UserContext);
-
+  console.log("on site opening", user, "         user role:", user.role);
   if (!user) {
     return (
       <>
