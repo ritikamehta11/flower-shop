47aa24e868ced00bddc46494479ddbb90aec3fb4
Ritika Mehta <mehtaritika@gmail.com>
2024-10-17 13:01:26 -0400

    header navigation modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,64 @@
+import { UserContext } from "@/context/UserContext";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <nav className='flex flex-wrap flex-row  basis-2/4  w-4/5 ' >
-      <ul className='flex flex-wrap gap-5 justify-evenly basis-2/4 content-center'>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/contact'>Contact Us</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/Products'>All Products</Link></li>
+  const { user } = useContext(UserContext);
 
-      </ul>
-      <ul className='flex flex-wrap gap-5  justify-end basis-2/4 content-center'>
-        <li><Link to='/cart'>Cart</Link></li>
-        <li><Link to='/register'>Register</Link></li>
-      </ul>
-    </nav>
-  )
+  if (!user) {
+    return (
+      <>
+
+        <nav className='flex flex-wrap flex-row  basis-2/4  w-4/5 ' >
+          <ul className='flex flex-wrap gap-5 justify-evenly basis-2/4 content-center'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/contact'>Contact Us</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/Products'>All Products</Link></li>
+
+          </ul>
+          <ul className='flex flex-wrap gap-5  justify-end basis-2/4 content-center'>
+            <li><Link to='/cart'></Link></li>
+            <li><Link to='/register'>Register</Link></li>
+          </ul>
+        </nav></>
+    )
+  }
+  else if (user.role === "user") {
+    return (
+
+      <>
+
+        <nav className='flex flex-wrap flex-row  basis-2/4  w-4/5 ' >
+          <ul className='flex flex-wrap gap-5 justify-evenly basis-2/4 content-center'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/contact'>Contact Us</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/Products'>All Products</Link></li>
+
+          </ul>
+          <ul className='flex flex-wrap gap-5  justify-end basis-2/4 content-center'>
+            <li><Link to='/cart'>Cart</Link></li>
+            <li><Link to='/register'>Register</Link></li>
+          </ul>
+        </nav></>
+    )
+  }
+
+
+  else if (user.role === "admin") {
+    <>
+
+      <nav className='flex flex-wrap flex-row  basis-2/4  w-4/5 ' >
+        <ul className='flex flex-wrap gap-5 justify-evenly basis-2/4 content-center'>
+          <li><Link to='/admin/dashboard'>DashBoard</Link></li>
+          <li><Link to='/admin/allproducts'>All Products</Link></li>
+
+        </ul>
+        <ul className='flex flex-wrap gap-5  justify-end basis-2/4 content-center'>
+          <li><Link to='/cart'></Link></li>
+          <li><Link to='/register'>Register</Link></li>
+        </ul>
+      </nav></>
+  }
 }
