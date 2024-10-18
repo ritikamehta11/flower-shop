ef22bde8fcfe1cbc71b5b3d544327fe40362d8a8
Ritika Mehta <mehtaritika@gmail.com>
2024-10-18 00:28:31 -0400

    debugging cart.jsx

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -72,7 +72,7 @@ export const UserProvider = ({ children }) => {
       // Send the DELETE request to the backend
       console.log("id of the product:",productId._id);
       const response = await axios.delete(
-        `https://flower-shop-backend-81tw.onrender.com/api/cart/${user._id}/product/${productId}`,
+        `https://flower-shop-backend-81tw.onrender.com/api/cart/${user.id}/product/${productId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
