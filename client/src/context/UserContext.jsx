52ea1999b82946648344a27185e7b87bef8a6b2e
Ritika Mehta <mehtaritika@gmail.com>
2024-10-18 21:24:00 -0400

    debugging cartcontroller quantity

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -111,7 +111,7 @@ export const UserProvider = ({ children }) => {
         },
       });
       // Optionally refetch the cart or adjust the cart state accordingly
-      setCart({ ...cart, items: updatedCart });
+      setCart(response.data);
       // await fetchLatestCartData();
     } catch (error) {
       console.error('Error increasing quantity', error);
