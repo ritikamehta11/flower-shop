f34fd916aa109d5993c4b60cb0e1485b3e8460a1
Ritika Mehta <mehtaritika@gmail.com>
2024-12-17 13:40:19 -0500

    userid error

--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -3,11 +3,16 @@ import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export const Logout = () => {
-  const { setUser } = useContext(UserContext);
+  const { setUser ,setCart} = useContext(UserContext);
   const navigate = useNavigate();
   useEffect(() => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("cart");
+
     setUser(null);
+    setCart(null);
+
     navigate('/login');
   });
  
